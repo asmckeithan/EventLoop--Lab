c9ee2a776c772d104f826352cdc82511b6589681
asmckeithan <annesha.mckeithan@gmail.com>
2024-05-07 19:53:06 -0400

    Global variable declared

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Event Loop')
+//Declare a global counter variable.
+const count = 0;
+
+// Create a simple function that increments the variable, and then calls itself recursively.
+function counter(){
+    count++;
+    counter();
+}
+
+
+// Surround the initial function call in a try/catch block.
+// Within the catch, log the error and the value of the counter variable.
